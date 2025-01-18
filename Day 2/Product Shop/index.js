266b3fc9ad74c2ebe61109c52012359e3c972737
Yousef Abdelrhem <gmhammer99@gmail.com>
2025-01-18 21:30:09 +0200

    update

--- Day 2/Product Shop/index.js
+++ Day 2/Product Shop/index.js
@@ -1,6 +1,7 @@
 let result;
+let filteredArr = [];
 let cardSection = document.querySelector(".Card-Section");
-
+let SelectedCheckBox = [];
 document.addEventListener("DOMContentLoaded", () => {
   let loading = document.querySelector("#loading");
   let errMsg = document.querySelector("#errMsg");
@@ -19,18 +20,19 @@ async function Cards() {
     }
 
     loading.style.display = "none";
-    result.forEach(function (e, i) {
-      cardSection.innerHTML += `<div class="Card">
-      <div class="CardContainer">
-          <img src="${result[i].image}">
-          <div class="card-details">
-            <p class="cardCategory">${result[i].category}</p>
-            <p>Lorem ipsum dolor sit, </p>
-              <span>${result[i].price} $</span>
-          </div>
-        </div>
-      </div>`;
-    });
+    // result.forEach(function (e, i) {
+    //   cardSection.innerHTML += `<div class="Card">
+    //   <div class="CardContainer">
+    //       <img src="${result[i].image}">
+    //       <div class="card-details">
+    //         <p class="cardCategory">${result[i].category}</p>
+    //         <p class= "cardDescription">${result[i].description}</p>
+    //           <span>${result[i].price} $</span>
+    //       </div>
+    //     </div>
+    //   </div>`;
+    // });
+    insertCard(result);
   } catch (error) {
     errMsg.innerText = error.message;
     errMsg.style.display = "block";
@@ -42,40 +44,63 @@ Cards();
 document
   .querySelector(".inputCategories")
   .addEventListener("click", function (e) {
-    let checkBox = e.target;
-
-    if (checkBox.checked) {
-      console.log(checkBox.value);
-      let newArr = result.filter((e, i) => {
-        if (checkBox.value == result[i].category) {
-          return result[i].category;
-        }
+    if (e.target.type == "checkbox") {
+      let checkBox = e.target;
+      updateCheckedBox(checkBox);
+      filteredArr = result.filter((item) => {
+        return (
+          SelectedCheckBox.length === 0 || checkBox.value == "all" || SelectedCheckBox.includes(item.category)
+        );
       });
-
-      RemoveCards();
-
-      console.log(newArr);
-      newArr.forEach(function (e, i) {
-        cardSection.innerHTML += `
-        <div class="Card">
-        <div class="CardContainer">
-            <img src="${result[i].image}">
-            <div class="card-details">
-              <p class="cardCategory">${result[i].category}</p>
-              <p>Lorem ipsum dolor sit, </p>
-                <span>${result[i].price} $</span>
-            </div>
-          </div>
-        </div>`;
-      });
+      cardSection.innerHTML = "";
+      insertCard(filteredArr);
     }
   });
 
-// for flags for the filter is of them is true then update the if
+function updateCheckedBox(checkBox) {
+  if (checkBox.checked) {
+    SelectedCheckBox.push(checkBox.value);
+  } else {
+    SelectedCheckBox = SelectedCheckBox.filter((item) => {
+      return !(item === checkBox.value);
+    });
+  }
+}
 
-function RemoveCards() {
-  let cards = document.querySelectorAll(".Card");
-  cards.forEach((e) => {
-    e.remove();
+function insertCard(arr) {
+  arr.forEach(function (e, i) {
+    let shortenDec = e.description.length > 60
+      ? e.description.substring(0, 70) + "..."
+      : e.description;
+
+    cardSection.innerHTML += `
+      <div class="Card">
+      <div class="CardContainer">
+          <img src="${arr[i].image}">
+          <div class="card-details">
+            <p class="cardCategory">${arr[i].category}</p>
+            <p>${shortenDec}</p>
+              <span>${arr[i].price} $</span>
+          </div>
+        </div>
+      </div>`;
   });
+}
+
+document.querySelector("select").addEventListener("input", function (e) {
+  let val = e.target.value;
+  if (filteredArr.length > 0) {
+    SortingCards(filteredArr, val);
+  } else {
+    SortingCards(result, val);
+    console.log("true");
+  }
+});
+
+function SortingCards(arr, val) {
+  let sortedArr = [...arr].sort((a, b) => {
+    return val === "asc" ? a.price - b.price : b.price - a.price;
+  });
+  cardSection.innerHTML = "";
+  insertCard(sortedArr);
 }
